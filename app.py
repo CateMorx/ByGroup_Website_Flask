c9b316787332d73c54f5da7b2f2b10835d143559
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-12-11 22:54:31 +0800

    added lab act 1

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, flash
 from math import pi
+import timeit
+from exponential_search import exponential_search, exponential_search_wrapper
+from binary_search import binary_search, binary_search_wrapper
+from interpolation_search import interpolation_search, interpolation_search_wrapper
+from jump_search import jump_search, jump_search_wrapper
+from linear_search import linear_search, linear_search_wrapper
+from ternary_search import ternary_search, ternary_search_wrapper
+
 
 app = Flask(__name__)
 
@@ -16,13 +24,52 @@ def works():
     return render_template('works.html')
 
 @app.route('/SearchAlgo', methods=['GET', 'POST']) #Ito yung iibahin natin for Lab Act 1
-def area_of_a_circle():
-    result = None
-    if request.method == 'POST':
-        input_string = request.form.get('inputString', '')
-        input_string = float(input_string)
-        result = str(pi * input_string ** 2)
-    return render_template('SearchAlgo.html', result=result)
+def search_algo():
+    numbers = range(1, 101)
+    test_data = ", ".join(map(str, numbers))
+    #print(test_data)
+    if request.method == "POST":
+        array_str = request.form.get("array")
+        target_str = request.form.get("target")
+        search_type = request.form.get("search_type")
+
+        try:
+            array = list(map(int, array_str.split(",")))
+            target = int(target_str)
+            low, high = 0, len(array) - 1
+
+            result = -1  # Initialize result before the conditional statements
+
+            if search_type == "exponential":
+                execution_time = timeit.timeit("exponential_search_wrapper(exponential_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
+                result = exponential_search_wrapper(binary_search, array, target)
+                # result = exponential_search(array, target)
+            elif search_type == "binary":
+                #arr = list(map(int, array_str.split(",")))
+                execution_time = timeit.timeit("binary_search_wrapper(binary_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
+                result = binary_search_wrapper(binary_search, array, target)
+            elif search_type == "interpolation":
+                execution_time = timeit.timeit("interpolation_search_wrapper(interpolation_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
+                result = interpolation_search_wrapper(interpolation_search, array, target)                
+                # result = interpolation_search(array, target)
+            elif search_type == "jump":
+                execution_time = timeit.timeit("jump_search_wrapper(jump_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
+                result = jump_search_wrapper(interpolation_search, array, target)  
+                # result = jump_search(array, target)
+            elif search_type == "linear":
+                execution_time = timeit.timeit("linear_search_wrapper(linear_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
+                result = linear_search_wrapper(linear_search, array, target)  
+                # result = linear_search(array, target)
+            elif search_type == "ternary":
+                execution_time = timeit.timeit("ternary_search_wrapper(ternary_search, array, target, low, high)", globals={**globals(), "array": array, "target": target,"low":low,"high":high}, number=1)  * 1000 
+                result = ternary_search_wrapper(ternary_search, array, target, low, high)  
+                # result = ternary_search(array, target, low, high)
+
+            return render_template('SearchAlgo.html', result=result, search_type=search_type, execution_time=execution_time,test_data=test_data)
+        except ValueError:
+            return render_template('SearchAlgo.html', error="Invalid input. Ensure the array and target are integers.")
+    
+    return render_template('SearchAlgo.html', test_data=test_data)
 
 @app.route('/contact')
 def contact():
